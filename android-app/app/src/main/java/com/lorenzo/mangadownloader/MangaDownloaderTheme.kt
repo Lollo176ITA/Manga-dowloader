2acac6f8e6a1c2807290ccc333831d0a66b57423
Lollo176ITA <116870391+Lollo176ITA@users.noreply.github.com>
2026-04-22 13:08:26 +0200

    feat: better dark mode

--- android-app/app/src/main/java/com/lorenzo/mangadownloader/MangaDownloaderTheme.kt
+++ android-app/app/src/main/java/com/lorenzo/mangadownloader/MangaDownloaderTheme.kt
@@ -1,11 +1,14 @@
 package com.lorenzo.mangadownloader
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -33,8 +36,13 @@ private val AppDarkColorScheme = darkColorScheme(
 
 @Composable
 fun MangaDownloaderTheme(content: @Composable () -> Unit) {
+    val context = LocalContext.current
     val view = LocalView.current
-    val colorScheme = AppDarkColorScheme
+    val colorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        dynamicDarkColorScheme(context)
+    } else {
+        AppDarkColorScheme
+    }
 
     if (!view.isInEditMode) {
         SideEffect {
